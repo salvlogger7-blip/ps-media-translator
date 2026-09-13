659edcf30e4ced703ba7c5f90d7e1744cfd2cfc1
salvlogger7-blip <salvlogger7@gmail.com>
2026-09-13 20:59:27 +0700

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -3,10 +3,3 @@
 set -o errexit
 
 pip install -r requirements.txt
-
-# ទាញយក FFmpeg សម្រាប់ប្រើលើ Server
-mkdir -p ffmpeg
-cd ffmpeg
-wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
-tar xvf ffmpeg-release-amd64-static.tar.xz --strip-components=1
-cd ..
